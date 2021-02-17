dbe61aad52e839a4138983a05f23798c3a220d60
Yuto Takano <moa17stock@gmail.com>
2021-02-17 12:21:11 +0000

    Clean up and comment the entire main.lua file

--- main.lua
+++ main.lua
@@ -4,33 +4,44 @@ local gameSDK = require 'lua-discordGameSDK'
 
 local app
 
--- set [options]
-local o = {
-	rpc_wrapper = "lua-discordGameSDK",
-	periodic_timer = 10,
-	-- Recommendation value, to set `periodic_timer`:
-	-- value >= 1 second, if use lua-discordRPC,
-	-- value <= 15 second, because discord-rpc updates every 15 seconds.
-	playlist_info = "yes",
-	-- Valid value to set `playlist_info`: (yes|no)
-	loop_info = "yes",
-	-- Valid value to set `loop_info`: (yes|no)
-	cover_art = "yes",
-	-- Valid value to set `cover_art`: (yes|no)
-	mpv_version = "yes",
-	-- Valid value to set `mpv_version`: (yes|no)
-	active = "no",
-	-- Set Discord RPC active automatically when mpv started.
-	-- Valid value to `set_active`: (yes|no)
-	key_toggle = "D",
-	-- Key for toggle active/inactive the Discord RPC.
-	-- Valid value to set `key_toggle`: same as valid value for mpv key binding.
-	-- You also can set it in input.conf by adding this next line (without double quote).
-	-- "D script-binding mpv_discordRPC/active-toggle"
-}
-options.read_options(o) -- added identifier
+--[=====[
+	Main Options.
+	Configure these as you wish. You can also pass these as command-line 
+	arguments by e.g. --script-opts=mpvcord-active=yes 
 
--- set `script_info`
+	periodic_timer: How often the script communicates with Game SDK.
+									Recommended 1 <= x <= 15.
+	playlist_info:  Whether to display the playlist information. 
+								  (yes | no)
+	loop_info:      Whether to display your loop status. 
+								  (yes | no)
+	mpv_version:    Whether to show the mpv version. 
+								  (yes | no)
+	activate:       Whether to activate script on launch. 
+								  (yes | no)
+	key_toggle:     Key to toggle script. Can also be set in input.conf.
+	client_id:      Discord developer client ID. Keep the LL.
+--]=====]
+
+local o = {
+	periodic_timer = 2,
+	playlist_info = "yes",
+	loop_info = "yes",
+	mpv_version = "yes",
+	active = "no",
+	key_toggle = "D",
+	client_id = 798647747678568488LL
+}
+
+
+--[=====[
+	Setup.
+	Gets necessary information to be used later. Also sets up the
+	keybinding according to options. 
+--]=====]
+
+options.read_options(o)
+
 local script_info = {
 	name = mp.get_script_name(),
 	description = "Discord Rich Presence integration for mpv.",
@@ -39,33 +50,68 @@ local script_info = {
 	version = "2.0.0",
 }
 
--- set `mpv_version`
+msg.info(script_info.description)
+
+msg.verbose("Running on options:")
+msg.verbose(string.format("periodic_timer : %s", o.periodic_timer))
+msg.verbose(string.format("playlist_info  : %s", o.playlist_info))
+msg.verbose(string.format("loop_info      : %s", o.loop_info))
+msg.verbose(string.format("mpv_version    : %s", o.mpv_version))
+msg.verbose(string.format("active         : %s", o.active))
+msg.verbose(string.format("key_toggle     : %s", o.key_toggle))
+
 local mpv_version = mp.get_property("mpv-version"):sub(5)
 
--- set `startTime`
 local startTime = os.time(os.date("*t"))
 
--- local client = baseclient.connect()
+-- Keybinding
+mp.add_key_binding(o.key_toggle, "active-toggle", function ()
+	o.active = o.active == "yes" and "no" or "yes"
+	local status = o.active == "yes" and "active" or "inactive"
+	mp.osd_message(("[%s] Status: %s"):format(script_info.name, status))
+	msg.verbose(string.format("Status: %s", status))
+end, {
+	repeatable = false
+})
+
+-- Initialise Discord Game SDK and keep its instance
+discord_instance = gameSDK.initialize(o.client_id)
+
+-- Call main() once, and keep it calling periodically.
+main()
+mp.add_periodic_timer(o.periodic_timer, main)
+
+
+--[=====[
+	Main loop.
+	Called every `o.periodic_timer`. Parses various mpv values, formats
+	them nicely, and sends it off to the game SDK.
+--]=====]
+
 local function main()
-	-- Default values
+
+	-- Return early if discord game sdk doesn't need to run
+	if o.active == "no" then
+		discord_instance = gameSDK.clearPresence(discord_instance)
+		return
+	end
+
+	-- Default values for presence.
 	local presence = {
-		-- pid = 9999,
-		-- state = "Second line",
-		-- details = "Third line",
-		-- start = startTime,
-		-- end_time = startTime + 60,
-		-- large_image = "",
-		-- large_text = "",
-		-- small_image = "",
-		-- small_text = "",
-		-- party_id = "",
-		-- party_size = 0,
-		-- party_max = 0
-		-- join = "",
-		-- spectate = "",
-		-- match = "",
-		-- buttons = {},
-		-- instance = false
+		state = "", -- Second line
+		details = "", -- Third line
+		start_time = startTime, -- Epoch
+		end_time = startTime + 60,
+		large_image = "", -- Image key
+		large_text = "",
+		small_image = "", -- Image key
+		small_text = "",
+		party_id = "",
+		party_size = 0,
+		party_max = 0
+		match_secret = "", -- Not exactly sure how to use them >.<
+		join_secret = "",
+		spectate_secret = ""
 	}
 
 	-- Set images
@@ -158,7 +204,7 @@ local function main()
 			presence["large_image"] = "radio"
 			presence["large_text"] = "Radio"
 			presence["details"] = "Listening to NHK R1"
-			state = "" -- hide (Playing 1x) because it is unnecessary
+			presence["state"] = "" -- hide (Playing 1x) because it is unnecessary
 
 		elseif string.match(url, "www.youtube.com/watch%?v=([a-zA-Z0-9-_]+)&?.*$") ~= nil or string.match(url, "youtu.be/([a-zA-Z0-9-_]+)&?.*$") ~= nil then
 			presence["large_image"] = "youtube"
@@ -192,40 +238,9 @@ local function main()
 		presence["end"] = nil
 	end
 
+	presence["spectate_secret"] = "https://test.com"
+
 	-- set game activity
-	if o.active == "yes" then
-		presence.details = presence.details:len() > 127 and presence.details:sub(1, 127) or presence.details
-		referencesTable = gameSDK.updatePresence(referencesTable, presence)
-	else
-		referencesTable = gameSDK.clearPresence(referencesTable)
-	end
+	presence.details = presence.details:len() > 127 and presence.details:sub(1, 127) or presence.details
+	referencesTable = gameSDK.updatePresence(referencesTable, presence)
 end
-
--- print script info
-msg.info(string.format(script_info.description))
--- msg.info(string.format("Upstream: %s", script_info.upstream))
--- msg.info(string.format("Version: %s", script_info.version))
-
--- print option values
-msg.verbose(string.format("periodic_timer : %s", o.periodic_timer))
-msg.verbose(string.format("playlist_info  : %s", o.playlist_info))
-msg.verbose(string.format("loop_info      : %s", o.loop_info))
-msg.verbose(string.format("cover_art      : %s", o.cover_art))
-msg.verbose(string.format("mpv_version    : %s", o.mpv_version))
-msg.verbose(string.format("active         : %s", o.active))
-msg.verbose(string.format("key_toggle     : %s", o.key_toggle))
-
--- toggling active or inactive
-mp.add_key_binding(o.key_toggle, "active-toggle", function()
-		o.active = o.active == "yes" and "no" or "yes"
-		local status = o.active == "yes" and "active" or "inactive"
-		mp.osd_message(("[%s] Status: %s"):format(script_info.name, status))
-		msg.info(string.format("Status: %s", status))
-	end,
-	{repeatable=false})
-
-local clientId = 798647747678568488LL
-referencesTable = gameSDK.initialize(clientId)
-
--- run `main` function
-mp.add_periodic_timer(o.periodic_timer, main)
